7bcac197b10cf8ceb6be1e6823c3adac06c346bb
Nelluq <cullenjski@gmail.com>
2019-07-14 13:36:34 -0400

    Removed polygon checker function

--- power_analyzer.py
+++ power_analyzer.py
@@ -137,28 +137,7 @@ class PowerNetAnalyzerGui(wx.Frame):
         if self.source_row != -1:
             self.run_analysis()
 
-    # determine if a point is inside a given polygon or not
-    # Polygon is a list of (x,y) pairs.
-    # copied from http://www.ariel.com.au/a/python-point-int-poly.html
-    def point_inside_polygon(self,x,y,poly):
-
-        n = len(poly)
-        inside =False
-
-        p1x,p1y = poly[0]
-        for i in range(n+1):
-            p2x,p2y = poly[i % n]
-            if y > min(p1y,p2y):
-                if y <= max(p1y,p2y):
-                    if x <= max(p1x,p2x):
-                        if p1y != p2y:
-                            xinters = (y-p1y)*(p2x-p1x)/(p2y-p1y)+p1x
-                        if p1x == p2x or x <= xinters:
-                            inside = not inside
-            p1x,p1y = p2x,p2y
-
-        return inside
-
+    # TODO: move this to its own class
     def run_analysis(self):
         print("Running Analysis of net: {}".format(self.analysis_netname))
 
